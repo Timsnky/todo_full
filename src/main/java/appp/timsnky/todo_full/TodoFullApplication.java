0b01c411df0e0ec8dcd160f79f631e5fe3c67232
Timothy Kimathi <timsnky@gmail.com>
2019-06-05 10:07:40 +0300

    Initialised application

---
+++ src/main/java/appp/timsnky/todo_full/TodoFullApplication.java
@@ -0,0 +1,13 @@
+package appp.timsnky.todo_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoFullApplication.class, args);
+    }
+
+}
